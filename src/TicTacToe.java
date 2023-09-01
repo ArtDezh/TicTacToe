8e9a0c8fab54fa62df442ff713d4b5e52881e653
ArturD <artur2134@mail.ru>
2023-09-01 16:19:22 +0300

    Console game TicTackToe

---
+++ src/TicTacToe.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Для выбора ячейки используйте числа от 1 до 9 в соответствии с таблицей ниже!");
+        printTable();
+        if (new Random().nextBoolean()) {
+            makeComputerMove();
+            printGameTable();
+        }
+        while (true) {
+            makeUserMove();
+            printGameTable();
+            if (isUserWin()) {
+                System.out.println("YOU WIN!!!");
+                break;
+            }
+            if (isDraw()) {
+                System.out.println("Sorry, DRAW!!!");
+                break;
+            }
+            makeComputerMove();
+            printGameTable();
+            if (isComputerWin()) {
+                System.out.println("COMPUTER WIN!!!");
+                break;
+            }
+            if (isDraw()) {
+                System.out.println("Sorry, DRAW!!!");
+                break;
+            }
+        }
+        System.out.println("GAME OVER!!!");
+    }
+
+    private static void printGameTable() {
+
+    }
+
+    private static void printTable() {
+
+    }
+
+    private static void makeUserMove() {
+
+    }
+
+    private static void makeComputerMove() {
+
+    }
+
+    private static boolean isUserWin() {
+        return false;
+    }
+
+    private static boolean isComputerWin() {
+        return false;
+    }
+
+    private static boolean isDraw() {
+        return false;
+    }
+}
